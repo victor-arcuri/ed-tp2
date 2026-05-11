a0ce4a90a6449929a86f3fff00b160d22072ac06
Victor Romano Arcuri <victorromanoarcuri@ufmg.br>
2026-05-11 17:42:47 -0300

    feat: implementa lógica principal de todos os métodos com exceção de remover seguido, sem tratar de outputs

--- include/Grafo.hpp
+++ include/Grafo.hpp
@@ -199,6 +199,97 @@ public:
         this->tipo = novo_tipo; 
     }
 
+    Lista<No*> obter_vizinhos_apontados_por(int id_interno_grafo) const{
+        if (tipo == GRAFO_MATRIZ){
+            Lista<No*> vizinhos;
+            const Lista<int>& vizinhosMatriz = (*matriz)[id_interno_grafo];
+            for (int i = 0; i < vizinhosMatriz.get_tamanho(); i++){
+                if (vizinhosMatriz[i] == 1) vizinhos.inserir(nos[i]); 
+            }
+            return vizinhos;
+        } else if (tipo == GRAFO_LISTA){
+            return (*lista)[id_interno_grafo];
+        }
+        return Lista<No*>();
+    }
+
+    Lista<No*> obter_vizinhos_que_apontam_para(int id_iterno_grafo) const{
+        if (tipo == GRAFO_MATRIZ){
+            Lista<No*> vizinhos;
+
+            for (int i = 0; i < nos.get_tamanho(); i++){
+                if ((*matriz)[i][id_iterno_grafo] == 1) vizinhos.inserir(nos[i]);
+            }
+            return vizinhos;
+        } else if (tipo == GRAFO_LISTA){
+            Lista<No*> vizinhos;
+            for (int i = 0; i < nos.get_tamanho(); i++){
+                const Lista<No*>& vizinhos_do_elemento = (*lista)[i];
+                for (int j = 0; j < vizinhos_do_elemento.get_tamanho(); j++){
+                    if (vizinhos_do_elemento[j]->id_grafo == id_iterno_grafo){
+                        vizinhos.inserir(nos[i]);
+                        break;
+                    }
+                }
+            }
+            return vizinhos;
+        }
+        return Lista<No*>();
+    }
+
+    Lista<No*> obter_vizinhos_bidirecional(int id_iterno_grafo) const {
+        Lista<No*> vizinhos;
+
+        if (tipo == GRAFO_MATRIZ) {
+            const Lista<int>& linha = (*matriz)[id_iterno_grafo];
+            
+            for (int i = 0; i < nos.get_tamanho(); i++) {
+                if (linha[i] == 1 && (*matriz)[i][id_iterno_grafo] == 1) {
+                    vizinhos.inserir(nos[i]);
+                }
+            }
+        } 
+        else if (tipo == GRAFO_LISTA) {
+            const Lista<No*>& seguidos_1 = (*lista)[id_iterno_grafo];
+            
+            for (int i = 0; i < seguidos_1.get_tamanho(); i++) {
+                No* possivel_bidirecional = seguidos_1[i];
+                int id_possivel = possivel_bidirecional->id_grafo;
+                
+                const Lista<No*>& seguidos_2 = (*lista)[id_possivel];
+                bool bidirecional = false;
+                
+                for (int j = 0; j < seguidos_2.get_tamanho(); j++) {
+                    if (seguidos_2[j]->id_grafo == id_iterno_grafo) {
+                        bidirecional = true;
+                        break;
+                    }
+                }
+                
+                if (bidirecional) {
+                    vizinhos.inserir(possivel_bidirecional);
+                }
+            }
+        }
+        return vizinhos;
+    }
+
+    bool checar_aresta(int id_iterno_grafo1, int id_interno_grafo2) const {
+        if (tipo == GRAFO_MATRIZ) {
+            const Lista<int>& linha1 = (*matriz)[id_iterno_grafo1];
+            if (linha1[id_interno_grafo2] == 1) return true;
+        } 
+        else if (tipo == GRAFO_LISTA) {
+            const Lista<No*>& vizinhos = (*lista)[id_iterno_grafo1];
+            for (int i = 0; i < vizinhos.get_tamanho(); i++){
+                if (vizinhos[i]->id_grafo == id_interno_grafo2){
+                    return true;
+                }
+            }    
+        }
+        return false;
+    }
+
 };
 
 #endif
--- src/Sistema.cpp
+++ src/Sistema.cpp
@@ -11,15 +11,99 @@ Sistema::Sistema(TipoGrafo tipoInicial):    grafo_temas(tipoInicial),
 
 Sistema::~Sistema(){};
 
-void Sistema::alterar_armazenamento(TipoGrafo tipo){};
-const int Sistema::adicionar_usuario(const std::string& nome, int idade, Lista<int> tema_ids){};
-const int Sistema::adicionar_tema(const std::string& nome, TipoTema tipo){};
-void Sistema::seguir_usuario(int id1, int id2){};
+void Sistema::alterar_armazenamento(TipoGrafo tipo){
+    grafo_social.trocar_tipo(tipo);
+    grafo_temas.trocar_tipo(tipo);
+};
+
+const int Sistema::adicionar_usuario(const std::string& nome, int idade, Lista<int> tema_ids){
+    Usuario usuario = Usuario(num_usuarios, nome, idade);
+    usuarios.inserir(usuario);
+    int id_interno_social = grafo_social.criar_no(USUARIO, num_usuarios);
+    int id_interno_temas = grafo_temas.criar_no(USUARIO, num_usuarios);
+    
+    dicionario.registrar_usuario_social(id_interno_social);
+    dicionario.registrar_usuario_temas(id_interno_temas);
+
+    for (int i = 0; i < tema_ids.get_tamanho(); i++){
+        int id_tema_desejado = tema_ids[i];
+        const No& no_tema = dicionario.get_no_tema_grafo_temas(id_tema_desejado);
+        grafo_temas.criar_aresta(id_interno_temas, no_tema.id_grafo, false);
+    }
+    int id_criado = num_usuarios;
+    num_usuarios++;
+    return id_criado;
+};
+
+const int Sistema::adicionar_tema(const std::string& nome, TipoTema tipo){
+    Tema tema(num_temas, nome, tipo);
+    temas.inserir(tema);
+    int id_interno_temas = grafo_temas.criar_no(TEMA, num_temas);
+    dicionario.registrar_tema_temas(id_interno_temas);
+    int id_criado = num_temas;
+    num_temas++;
+    return id_criado;
+};
+
+void Sistema::seguir_usuario(int id1, int id2){
+    const No& no1 = dicionario.get_no_usuario_grafo_social(id1);
+    const No& no2 = dicionario.get_no_usuario_grafo_social(id2);
+    grafo_social.criar_aresta(no1.id_grafo, no2.id_grafo, false);
+};
+
 void Sistema::remover_seguimento_usuario(int id1, int id2){};
-void Sistema::consultar_temas(int id_usuario) const{};
-void Sistema::consultar_seguidores(int id_usuario) const{};
-void Sistema::consultar_seguidos(int id_usuario) const{};
-void Sistema::consultar_amigos(int id_usuario) const{};
-void Sistema::consultar_relacao(int id1, int id2) const{};
-void Sistema::consultar_interesse(int id_usuario, int id_tema) const{};
-void Sistema::consultar_popularidade(int id_tema) const{};
+
+void Sistema::consultar_temas(int id_usuario) const{
+    const No& no_usuario = dicionario.get_no_usuario_grafo_temas(id_usuario);
+    int id_interno = no_usuario.id_grafo;
+    Lista<No*> temas = grafo_temas.obter_vizinhos_apontados_por(id_interno);
+};
+
+void Sistema::consultar_seguidores(int id_usuario) const{
+    const No& no_usuario = dicionario.get_no_usuario_grafo_social(id_usuario);
+    int id_interno = no_usuario.id_grafo;
+    Lista<No*> seguidores = grafo_social.obter_vizinhos_que_apontam_para(id_interno);
+};
+void Sistema::consultar_seguidos(int id_usuario) const{
+    const No& no_usuario = dicionario.get_no_usuario_grafo_social(id_usuario);
+    int id_interno = no_usuario.id_grafo;
+    Lista<No*> seguidos = grafo_social.obter_vizinhos_apontados_por(id_interno);
+};
+void Sistema::consultar_amigos(int id_usuario) const{
+    const No& no_usuario = dicionario.get_no_usuario_grafo_social(id_usuario);
+    int id_interno = no_usuario.id_grafo;
+    Lista<No*> amigos = grafo_social.obter_vizinhos_bidirecional(id_interno);
+
+};
+void Sistema::consultar_relacao(int id1, int id2) const{
+    const No& no_usuario1 = dicionario.get_no_usuario_grafo_social(id1);
+    int id_interno1 = no_usuario1.id_grafo;
+    const No& no_usuario2 = dicionario.get_no_usuario_grafo_social(id2);
+    int id_interno2 = no_usuario2.id_grafo;
+    bool primeiro_segue_segundo = grafo_social.checar_aresta(id_interno1, id_interno2);
+    bool segundo_segue_primeiro = grafo_social.checar_aresta(id_interno2, id_interno1);
+    int caso;
+    if (primeiro_segue_segundo && segundo_segue_primeiro){
+        caso = 3;
+    } else if (primeiro_segue_segundo){
+        caso = 1;
+    } else if (segundo_segue_primeiro){
+        caso = 2;
+    } else {
+        caso = 0;
+    }
+};
+void Sistema::consultar_interesse(int id_usuario, int id_tema) const{
+    const No& no_usuario = dicionario.get_no_usuario_grafo_temas(id_usuario);
+    int id_interno_usuario = no_usuario.id_grafo;
+    const No& no_tema = dicionario.get_no_tema_grafo_temas(id_tema);
+    int id_interno_tema = no_tema.id_grafo;
+    bool possui_interesse = grafo_temas.checar_aresta(id_interno_usuario, id_interno_tema);
+};
+
+void Sistema::consultar_popularidade(int id_tema) const{
+    const No& no_tema = dicionario.get_no_tema_grafo_temas(id_tema);
+    int id_interno_tema = no_tema.id_grafo;
+    Lista<No*> interessados = grafo_temas.obter_vizinhos_que_apontam_para(id_interno_tema);
+    int quant_interessados = interessados.get_tamanho();
+};
