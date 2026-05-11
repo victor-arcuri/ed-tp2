153c19a5af866ada9d80438cbf3f90689dc04cfb
Victor Romano Arcuri <victorromanoarcuri@ufmg.br>
2026-05-11 15:29:49 -0300

    feat: implementa o TAD Tema

---
+++ include/Tema.hpp
@@ -0,0 +1,28 @@
+#ifndef TEMA_HPP
+#define TEMA_HPP
+
+#include <string>
+
+enum TipoTema {
+    ACADEMICO,
+    PESSOAL
+};
+
+class Tema {
+    int id;
+    std::string nome;
+    TipoTema tipo;
+
+public:
+    Tema(int id, const std::string& nome, TipoTema tipo);
+    ~Tema();
+    
+    int get_tipo() const;
+    int get_id() const;
+    const std::string& get_nome() const;
+
+    void set_tipo(TipoTema tipo);
+    void set_nome(const std::string& nome);
+};
+
+#endif
---
+++ src/Tema.cpp
@@ -0,0 +1,20 @@
+#include "Tema.hpp"
+Tema::Tema(int id, const std::string& nome, TipoTema tipo): id(id), nome(nome), tipo(tipo){};
+Tema::~Tema(){};
+
+int Tema::get_tipo() const{
+    return tipo;
+};
+int Tema::get_id() const{
+    return id;
+};
+const std::string& Tema::get_nome() const{
+    return nome;
+};
+
+void Tema::set_tipo(TipoTema tipo){
+    this->tipo = tipo;
+};
+void Tema::set_nome(const std::string& nome){
+    this->nome = nome;
+};
