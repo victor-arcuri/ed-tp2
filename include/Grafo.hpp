e5416c954a783daed97ba56614843a377036afe6
victor-arcuri <victor@arcuri.dev>
2026-05-10 21:02:10 -0300

    feat: adiciona TADS Lista e a base do Grafo

---
+++ include/Grafo.hpp
@@ -0,0 +1,157 @@
+#ifndef GRAFO_HPP
+#define GRAFO_HPP
+
+#include <stdexcept>
+#include "Lista.hpp"
+
+#define MATRIZ_DIMENSOES_INICIAIS 0
+
+#define LISTA_TAMANHO_INICIAL 0
+
+class MatrizAdjacencia {
+    int dimensoes;
+    Lista<Lista<int>> dados;
+
+public:
+    MatrizAdjacencia(int tamanho_inicial = MATRIZ_DIMENSOES_INICIAIS) : dimensoes(tamanho_inicial) {
+        for (int i = 0; i < dimensoes; i++) {
+            Lista<int> linha_vazia;
+            for (int j = 0; j < dimensoes; j++){
+                linha_vazia.inserir(0);
+            }
+            dados.inserir(linha_vazia); 
+        }
+    };
+
+    void aumentar_dimensao(){
+        for (int i = 0; i < dimensoes; i++){
+            dados[i].inserir(0);
+        }
+        dimensoes++;
+        Lista<int> linha_vazia;
+        for (int i = 0; i < dimensoes; i++){
+            linha_vazia.inserir(0);
+        }
+        dados.inserir(linha_vazia);
+    }
+
+    void alterar_valor(int id1, int id2, int valor, bool bidirecional){
+        if (valor != 0 && valor != 1) throw std::invalid_argument("Erro: O valor da aresta deve ser 0 ou 1.");
+        dados[id1][id2] = valor;
+        if (bidirecional) dados[id2][id1] = valor;
+    }
+
+    int obter_valor(int id1, int id2){
+        if ((id1 < 0 || id1 >= dimensoes) || (id2 < 0 || id2 >= dimensoes)) throw std::out_of_range("Erro: Indíce(s) inválido(s).");
+        return dados[id1][id2];
+    }
+
+    Lista<int>& operator[](int index) {
+        if (index < 0 || index >= dimensoes) throw std::out_of_range("Erro: Indíce(s) inválido(s).");
+        return dados[index];
+    }
+
+    const Lista<int>& operator[](int index) const {
+        if (index < 0 || index >= dimensoes) throw std::out_of_range("Erro: Indíce(s) inválido(s).");
+        return dados[index];
+    }
+
+    ~MatrizAdjacencia() {};
+
+};
+
+enum TipoNo {
+    USUARIO,
+    TEMA
+};
+
+struct No {
+    TipoNo tipo;
+    int id_externo;
+    int id_grafo;
+};
+
+class ListaAdjacencia{
+    int tamanho;
+    Lista<Lista<No*>> nos;
+
+public:
+    ListaAdjacencia(int tamanho_inicial = LISTA_TAMANHO_INICIAL): tamanho(tamanho_inicial){
+        for (int i = 0; i < tamanho; i++) {
+            Lista<No*> linha_vazia;
+            nos.inserir(linha_vazia);
+        }
+    };
+    ~ListaAdjacencia() {};
+
+    void adicionar_aresta(No* no1, No* no2, bool bidirecional){
+        nos[no1->id_grafo].inserir(no2);
+        if (bidirecional) nos[no2->id_grafo].inserir(no1);
+    }
+
+    void aumentar_tamanho(){
+        Lista<No*> vizinhos_vazios; 
+        nos.inserir(vizinhos_vazios);
+        tamanho++;
+    }
+};
+
+enum TipoGrafo{
+    GRAFO_MATRIZ,
+    GRAFO_LISTA
+};
+
+class Grafo{
+    Lista<No*> nos;
+    int id_nos;
+
+    TipoGrafo tipo;
+    MatrizAdjacencia* matriz;
+    ListaAdjacencia* lista;
+
+public:
+    Grafo(TipoGrafo tipo): id_nos(0), tipo(tipo){
+        matriz = nullptr;
+        lista = nullptr;
+        
+        if (tipo == GRAFO_MATRIZ) {
+            matriz = new MatrizAdjacencia(0);
+        } else {
+            lista = new ListaAdjacencia(0);
+        }
+    };
+    ~Grafo(){
+        for (int i = 0; i < nos.get_tamanho(); i++) {
+            delete nos[i];
+        }
+        if (matriz != nullptr) delete matriz;
+        if (lista != nullptr) delete lista;
+    }
+    int criar_no(TipoNo tipo, int id){
+        No* novo_no = new No;
+        novo_no->id_externo = id;
+        novo_no->id_grafo = id_nos;
+        novo_no->tipo = tipo;
+        id_nos++;
+        nos.inserir(novo_no);
+        if (this->tipo == GRAFO_MATRIZ){
+            matriz->aumentar_dimensao();
+        } 
+        else if (this->tipo == GRAFO_LISTA){
+            lista->aumentar_tamanho();
+        }
+        return id_nos-1;
+    }
+
+    void criar_aresta(int id_grafo1, int id_grafo2, bool bidirecional){
+        if (this->tipo == GRAFO_MATRIZ){
+            matriz->alterar_valor(id_grafo1, id_grafo2, 1, bidirecional);
+        } 
+        else if (this->tipo == GRAFO_LISTA){
+            lista->adicionar_aresta(nos[id_grafo1], nos[id_grafo2], bidirecional);
+        }
+    }
+
+};
+
+#endif
---
+++ include/Lista.hpp
@@ -0,0 +1,66 @@
+#ifndef LISTA_HPP
+#define LISTA_HPP
+
+#include <stdexcept>
+
+#define CAPACIDADE_INICIAL 2
+#define FATOR_CRESCIMENTO 2
+
+template<typename T>
+class Lista{
+    int capacidade;
+    int tamanho;
+    T* dados;
+
+    void redimensionar(){
+        capacidade*=FATOR_CRESCIMENTO;
+        T* novos_dados = new T[capacidade];
+        for (int i = 0; i < tamanho; i++){
+            novos_dados[i] = dados[i];
+        }
+        delete[] dados;
+        dados = novos_dados;
+    };
+    
+    public:
+        Lista(): capacidade(CAPACIDADE_INICIAL), tamanho(0){
+            dados = new T[capacidade];
+        };
+        ~Lista(){
+            delete[] dados;
+        };
+        
+        int get_tamanho(){
+            return tamanho;
+        }
+
+        void inserir(const T& elemento){
+            if (tamanho == capacidade){
+                redimensionar();
+            }
+            dados[tamanho] = elemento;
+            tamanho++;
+        };
+        T& obter(int pos){
+            if (pos < 0 || pos >= tamanho) {
+                throw std::out_of_range("Erro: índice inválido!");
+            }
+            return dados[pos];
+        };
+
+        const T& obter(int pos) const {
+            if (pos < 0 || pos >= tamanho) {
+                throw std::out_of_range("Erro: índice inválido!");
+            }
+            return dados[pos];
+        };
+
+        T& operator[](int pos){
+            return obter(pos);
+        };
+        const T& operator[](int pos) const{
+            return obter(pos);
+        };
+};
+
+#endif
