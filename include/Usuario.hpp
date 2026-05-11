1fee70b01d4b9842d89193b139c01f171a69f771
Victor Romano Arcuri <victorromanoarcuri@ufmg.br>
2026-05-11 15:23:49 -0300

    feat: implementa o TAD Usuario

---
+++ include/Usuario.hpp
@@ -0,0 +1,23 @@
+#ifndef USUARIO_HPP
+#define USUARIO_HPP
+
+#include <string>
+
+class Usuario {
+    int id;
+    std::string nome;
+    int idade;
+
+public:
+    Usuario(int id, const std::string& nome, int idade);
+    ~Usuario();
+    
+    int get_idade() const;
+    int get_id() const;
+    const std::string& get_nome() const;
+
+    void set_idade(int idade);
+    void set_nome(const std::string& nome);
+};
+
+#endif
